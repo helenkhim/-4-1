3f89bbc32dfce72510c1d23171dfab5a8617b652
helenkhim <elenbogacheva97@yandex.ru>
2021-01-10 18:25:41 +0300

    добавление кода

---
+++ BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int cash = 20;
+        int price = 10000;
+        int miles = price / cash;
+        return miles;
+    }
+}
+
