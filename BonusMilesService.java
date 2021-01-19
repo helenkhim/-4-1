30fdad6ff9a9cebce1ab7968d2f19d1955613d5b
helenkhim <elenbogacheva97@yandex.ru>
2021-01-19 20:10:10 +0300

    изменения в файле BonusMilesService

--- BonusMilesService.java
+++ BonusMilesService.java
@@ -1,9 +1,9 @@
 public class BonusMilesService {
     public int calculate(int cost) {
         int cash = 20;
-        int price = 10000;
-        int miles = price / cash;
+        int miles = cost / cash;
         return miles;
     }
 }
+
 
